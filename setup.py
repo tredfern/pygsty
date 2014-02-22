59c5ab90e2dd2bafb9a281871ba31a185622fc72
Trevor Redfern <trevorredfern@gmail.com>
2014-02-21 21:41:01 -0800

    Created package for pygsty

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='pygsty',
+        version='0.1',
+        description='Library for constructing graphics applications',
+        url='http://github.com/tredfern/pygsty',
+        author='Trevor Redfern',
+        author_email='trevorredfern@gmail.com',
+        license='MIT',
+        packages=['pygsty'],
+        zip_safe=False)
