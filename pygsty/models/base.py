fa3661ca1608be131b47be9f7f89f1226ebb145b
Trevor Redfern <trevorredfern@gmail.com>
2014-03-02 10:09:50 -0800

    Polished up base models a bit more

--- pygsty/models/base.py
+++ pygsty/models/base.py
@@ -8,17 +8,23 @@ def render_models():
     default_batch.draw()
 
 class BaseModel():
-    def __init__(self):
-        self.moveTo(0, 0)
-        self._rotation = 0
-
-    @property
-    def rotation(self):
-        return self._rotation
+    def __init__(self, position=(0,0)):
+        self.moveTo(position[0], position[1])
+        self.rotation = 0
 
     @property
     def position(self):
         return self._position
+
+    @property
+    def x(self):
+        return self._position.x
+
+    @property
+    def y(self):
+        return self._position.y
+
+
 
     def moveTo(self, x, y):
         self._position = pygsty.euclid.Point2(x, y)
@@ -31,15 +37,15 @@ class VisibleModelGroup(pyglet.graphics.Group):
 
     def set_state(self):
         pyglet.gl.glPushMatrix()
-        pyglet.gl.glTranslatef(self.model.position.x, self.model.position.y, 0)
+        pyglet.gl.glTranslatef(self.model.screen_x, self.model.screen_y, 0)
         pyglet.gl.glRotatef(self.model.rotation, 0, 0, 1)
        
     def unset_state(self):
         pyglet.gl.glPopMatrix()
 
 class VisibleModel(BaseModel):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, position=(0, 0)):
+        super().__init__(position = position)
         self._render_group = VisibleModelGroup(self)
         self._batch = default_batch
 
@@ -50,3 +56,11 @@ class VisibleModel(BaseModel):
     @property
     def batch(self):
         return self._batch
+
+    @property
+    def screen_x(self):
+        return self.x
+
+    @property
+    def screen_y(self):
+        return self.y
--- test/test_models.py
+++ test/test_models.py
@@ -5,16 +5,20 @@ class TestBaseModel(unittest.TestCase):
     def test_models_start_at_position_zero_zero(self):
         m = pygsty.models.BaseModel()
         self.assertEqual( m.position, pygsty.euclid.Point2(0, 0) )
-
-    def test_models_start_with_no_rotation(self):
-        m = pygsty.models.BaseModel()
-        self.assertEqual( m.rotation, 0)
+        self.assertEqual( m.x, 0 )
+        self.assertEqual( m.y, 0 )
 
     def test_you_can_move_the_model(self):
         m = pygsty.models.BaseModel()
         m.moveTo(40, 50)
         self.assertEqual( m.position.x, 40 )
         self.assertEqual( m.position.y, 50 )
+        self.assertEqual( m.x, 40 )
+        self.assertEqual( m.y, 50 )
+
+    def test_you_can_instantiate_at_another_position(self):
+        m = pygsty.models.BaseModel(position=(10, 32))
+        self.assertEqual( m.position, (10, 32) )
 
 
 class TestVisibleModel(unittest.TestCase):
@@ -34,4 +38,8 @@ class TestVisibleModel(unittest.TestCase):
         self.assertEqual( model.batch, pygsty.models.default_batch )
 
 
+    def test_screen_coordinates_default_to_standard_position(self):
+        model = pygsty.models.VisibleModel(position=(30, 40) )
+        self.assertEqual(model.screen_x, 30 )
+        self.assertEqual(model.screen_y, 40 )
 
